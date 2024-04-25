b1bd8fdf01d71fa077079a851a97a07cf709b5ff
Miguel Ripoll <3296866+MiguelRipoll23@users.noreply.github.com>
2024-04-25 20:35:11 +0000

    changes

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,6 @@
-import { BrowserService } from "./services/browser-service.js";
 import { GameLoopService } from "./services/game-loop-service.js";
 
 const canvas = document.querySelector("#canvas") as HTMLCanvasElement;
-new BrowserService(canvas);
 
 const gameLoop = new GameLoopService(canvas);
 gameLoop.start();
--- static/js/main.js
+++ static/js/main.js
@@ -1,6 +1,4 @@
-import { BrowserService } from "./services/browser-service.js";
 import { GameLoopService } from "./services/game-loop-service.js";
 const canvas = document.querySelector("#canvas");
-new BrowserService(canvas);
 const gameLoop = new GameLoopService(canvas);
 gameLoop.start();

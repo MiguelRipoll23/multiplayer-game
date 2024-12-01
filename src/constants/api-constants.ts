e0fbd760ae0253703e1484a38cfc6f8ad5bc25c6
Miguel Ripoll <3296866+MiguelRipoll23@users.noreply.github.com>
2024-12-01 15:36:27 +0100

    fixes

--- src/constants/api-constants.ts
+++ src/constants/api-constants.ts
@@ -11,20 +11,19 @@ export const WEBSOCKET_BASE_URL =
   API_WS_PROTOCOL + API_HOSTNAME + API_PATH + API_VERSION;
 
 export const VERSION_ENDPOINT = "/version";
-export const REGISTRATION_OPTIONS_ENDPOINT = "/passkey/registration-options";
-export const VERIFY_REGISTRATION_RESPONSE_ENDPOINT =
-  "/passkey/registration-response/verify";
-export const AUTHENTICATION_OPTIONS_ENDPOINT =
-  "/passkey/authentication-options";
-export const VERIFY_AUTHENTICATION_RESPONSE_ENDPOINT =
-  "/passkey/authentication-response/verify";
+export const CREDENTIAL_ENDPOINT = "/credential";
+export const REGISTRATION_OPTIONS_ENDPOINT = `${CREDENTIAL_ENDPOINT}/registration-options`;
+export const VERIFY_REGISTRATION_RESPONSE_ENDPOINT = `${CREDENTIAL_ENDPOINT}/registration-response/verify`;
+export const AUTHENTICATION_OPTIONS_ENDPOINT = `${CREDENTIAL_ENDPOINT}/authentication-options`;
+export const VERIFY_AUTHENTICATION_RESPONSE_ENDPOINT = `${CREDENTIAL_ENDPOINT}/authentication-response/verify`;
 export const REGISTER_ENDPOINT = "/register";
 export const CONFIGURATION_ENDPOINT = "/configuration";
 export const WEBSOCKET_ENDPOINT = "/websocket";
 export const MESSAGES_ENDPOINT = "/messages";
-export const MATCHES_FIND_ENDPOINT = "/matches/find";
-export const MATCHES_ADVERTISE_ENDPOINT = "/matches/advertise";
-export const MATCHES_REMOVE_ENDPOINT = "/matches/remove";
+export const MATCHES_ENDPOINT = "/matches";
+export const MATCHES_FIND_ENDPOINT = `${MATCHES_ENDPOINT}/matches/find`;
+export const MATCHES_ADVERTISE_ENDPOINT = `${MATCHES_ENDPOINT}/advertise`;
+export const MATCHES_REMOVE_ENDPOINT = `${MATCHES_ENDPOINT}/remove`;
 export const SCOREBOARD_PATH = "/scoreboard";
 export const SCOREBOARD_SAVE_SCORE_PATH = `${SCOREBOARD_PATH}/saveScore`;
 export const SCOREBOARD_GET_RANKING_PATH = `${SCOREBOARD_PATH}/getRanking`;
